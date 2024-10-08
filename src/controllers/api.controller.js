0e02b681cc22390fb65dd07626217335f6a86578
Albert Batlle <albert.batllle@gmail.com>
2024-10-08 20:06:03 +0200

    volcar api a bbdd

--- src/controllers/api.controller.js
+++ src/controllers/api.controller.js
@@ -1,10 +1,35 @@
+const db = require("../config/dbMySQL.js");
+
 exports.getCreditCards = async(req, res) => {
+    let connection;
     // Fetch tiene una promesa
     // fetch().then().catch()
     try {
         const size = req.params.size;
         const response = await fetch("https://random-data-api.com/api/v2/credit_cards?size="+size);
         const responseJSON = await response.json();
+        // opción map
+        const values = responseJSON.map(credit_card => [
+            credit_card.id, 
+            credit_card.uid,
+            credit_card.credit_card_number,
+            credit_card.credit_card_expiry_date,
+            credit_card.credit_card_type,
+        ]);
+        connection = await db.getConnection();
+        await connection.query("insert into credit_cards values ?", [values]);
+
+        // opción bucle
+        // responseJSON.forEach(async credit_card => {
+        //     await connection.query("insert into credit_cards values (?,?,?,?,?)", [
+        //         credit_card.id, 
+        //         credit_card.uid,
+        //         credit_card.credit_card_number,
+        //         credit_card.credit_card_expiry_date,
+        //         credit_card.credit_card_type
+        //     ])
+        // });
+
         return res.status(200).json({
             message: "Consumo a la randomAPI correcta",
             credit_cards: responseJSON
--- src/sql/db.sql
+++ src/sql/db.sql
@@ -10,3 +10,11 @@ CREATE TABLE users (
     create_at timestamp default current_timestamp,
     update_at timestamp default current_timestamp on update current_timestamp
 );
+
+CREATE TABLE credit_cards (
+    id int primary key,
+    uid varchar(255) not null,
+    credit_card_number varchar(19) not null,
+    credit_card_expiry_date date not null,
+    credit_card_type varchar(100) not null
+);
